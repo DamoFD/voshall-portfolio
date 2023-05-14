89a5d601991c69e2277803eb14c063e5e907551a
DamoFD <voshall@yahoo.com>
2023-05-13 18:17:17 -0700

    mobile styling

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var menuToggle = document.querySelector(".menu-toggle");
-  
-    // Mobile Nav Toggle
-    if (menuToggle) {
-      menuToggle.addEventListener("click", function () {
+  var menuToggle = document.querySelector(".menu-toggle");
+  var navItems = document.querySelectorAll(".nav-item");
+
+  // Mobile Nav Toggle
+  if (menuToggle) {
+    menuToggle.addEventListener("click", function () {
+      var nav = document.querySelector(".nav");
+      nav.classList.toggle("mobile-nav");
+      this.classList.toggle("is-active");
+    });
+
+    navItems.forEach(function (navItem) {
+      navItem.addEventListener("click", function () {
         var nav = document.querySelector(".nav");
         nav.classList.toggle("mobile-nav");
-        this.classList.toggle("is-active");
+        menuToggle.classList.toggle("is-active");
       });
-    }
+    });
+  }
 });
